a50638a9c699b2014e61d1bf3409676325191851
kemo2002ldlkd <119129364+kemo2002ldlkd@users.noreply.github.com>
2025-02-07 14:21:41 +0200

    Add files via upload

---
+++ Assignment 01 EF/Assignment 01 EF/Company.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_01_EF_
+{
+    internal class Company(): base ()
+    {
+
+
+
+        public class CompanyDbContext : DbContext
+        {
+            public CompanyDbContext() : base()
+            {
+            }
+
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+                optionsBuilder.UseSqlServer("Server=.;Database=CompanyG84;Trust_Connection=true");
+            }
+
+            public DbSet<Employee> Employees { get; set; }
+        }
+
+
+    }
+
+
+
---
+++ Assignment 01 EF/Assignment 01 EF/Program.cs
@@ -0,0 +1,30 @@
+﻿//1- By Convention [Default Behaviour]
+internal class Employee
+{
+
+   
+    {
+        public int Id { get; set; }//public numeric Property Named As
+                                   //"Id", "EmployeeId"=>Pk identity (1,1)
+        public string? Name { get; set; }// nvarchar(max)
+                                         //.net 5: Refernce Type: Allow Null [optional]
+                                         //.net 6: Refernce Type: Not Allow Null [Required]
+                                         //Nullable<string>
+        public decimal Salary { get; set; } // decimal(18,2) 
+        public int? Age { get; set; }  // Nullable <int>
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
